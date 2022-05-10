182a7ec0cce6444565a5ca13cfbb0d8300fe908f
meinhoonharsh <harshprogrammer782@gmail.com>
2022-05-10 14:48:13 +0530

    Imported is_user_admin in AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,6 +9,7 @@ class AdminController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('is_user_admin');
     }
 
     public function index()
